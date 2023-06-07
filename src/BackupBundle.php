c46c10a8039034f217ae8458db25a4dde9235e0b
Frank Jeroma <53892311+localbranding-de@users.noreply.github.com>
2023-06-07 15:26:49 +0200

    Add files via upload

---
+++ src/BackupBundle.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * This file is part of [package name].
+ *
+ * (c) John Doe
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace LocalbrandingDe\BackupBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BackupBundle extends Bundle
+{
+
+}
